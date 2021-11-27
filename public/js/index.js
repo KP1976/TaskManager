f7edfdeed53c57cd5e67c2702ec4488983c0b13a
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2021-11-27 18:52:34 +0100

    Add animation to 'desktop-add-task' component on small desktop devices (width 1280px)

--- public/js/index.js
+++ public/js/index.js
@@ -8,9 +8,16 @@ const hamburgerMenuBars = document.querySelectorAll(
 const hamburgerMenuShortBar = document.querySelector(
   '.mobile-hamburger-menu__bar--short'
 );
+
 const addTaskPage = document.querySelector('.add-task');
 const addTaskButton = document.querySelector('.add-task-button');
 const addTaskBackButton = document.querySelector('.add-task__back-button');
+
+const desktopAddTaskButton = document.querySelector('.desktop-add-task-button');
+const desktopAddTaskComponent = document.querySelector('.desktop-add-task');
+const desktopTodoAndDoneTasks = document.querySelector(
+  '.desktop-tasks-todo-and-done'
+);
 
 const toggleMobileMenu = () => {
   mobileMenu.classList.toggle('menu-open');
@@ -20,9 +27,16 @@ const toggleMobileMenu = () => {
 };
 
 const showAddTaskPage = () => addTaskPage.classList.add('open');
+const showDesktopAddTaskPage = () => {
+  desktopAddTaskComponent.classList.add('show-add-task');
+  desktopTodoAndDoneTasks.style.transform = 'translateX(0%)';
+};
+
 const hideAddTaskPage = () => addTaskPage.classList.remove('open');
 
 mmobileHamburgerMenuButton.addEventListener('click', toggleMobileMenu);
 
 if (addTaskButton) addTaskButton.addEventListener('click', showAddTaskPage);
 addTaskBackButton.addEventListener('click', hideAddTaskPage);
+
+desktopAddTaskButton.addEventListener('click', showDesktopAddTaskPage);
