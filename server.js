71e88cbb14cdd44249f18df734e700884433ef2c
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-01-02 15:26:14 +0100

    Install and start using handlebars

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
+app.engine(
+  '.hbs',
+  engine({
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>');
+  res.render('header');
 });
 
 app.listen(PORT, 'localhost', () => {
