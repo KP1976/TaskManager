0d51d7935135c07d87f3badefff508ca107ef2ce
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-01-02 14:59:33 +0100

    Install eslint rules

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>');
+});
+
 app.listen(PORT, 'localhost', () => {
   console.log(`Server starts listening at http://localhost:${PORT}`);
 });
