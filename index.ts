d19eaba2994c88abdf40beb2b39be6f82547475e
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-04-03 10:44:49 +0200

    Add cors.

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
 import 'express-async-errors';
 import { json } from 'express';
+import * as cors from 'cors';
 import { handleError } from './utilities/errors';
 import { taskRouter } from './routes/task';
 const PORT = 3001;
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 
 app.use(json());
 app.use('/api/tasks', taskRouter);
