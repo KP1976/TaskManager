a4ea2d8d9f452a54a052217472292a727d7ef84e
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-08-27 12:59:58 +0200

    make it better

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'express';
 import cors from 'cors';
-import { handleError } from './utilities/errors';
+// import { handleError } from './utilities/errors';
 import { taskRouter } from './routes/task';
 import { config } from './config/config';
 
@@ -24,7 +24,7 @@ app.use(json());
 // app.use('/tasks', taskRouter);
 app.use('/api/tasks', taskRouter);
 
-app.use(handleError);
+// app.use(handleError);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on port ${PORT} on http://localhost:${PORT}`);
